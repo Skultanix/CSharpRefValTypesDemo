2b20fb3e13984afbcddf556d4dccdf2d886bd062
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-10-10 17:36:44 -0600

    Added explanation of what is going on in memory for the first example/

--- RefValTypesDemo/Program.cs
+++ RefValTypesDemo/Program.cs
@@ -7,7 +7,7 @@
             var a = 10;
             var b = a;
             b++;
-
+            //when location "a" is duplicated on stack to location "b", "b" exists independently of "a" which is why increment only affected "b"
             Console.WriteLine(String.Format("a: {0}, b: {1}", a, b));
         }
     }
