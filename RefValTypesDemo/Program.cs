51701654bf0b1db661f902d06a6e757da46a85da
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-10-10 17:43:36 -0600

    Second example completed, explanation of what is going on annotated in code.

--- RefValTypesDemo/Program.cs
+++ RefValTypesDemo/Program.cs
@@ -9,6 +9,12 @@
             b++;
             //when location "a" is duplicated on stack to location "b", "b" exists independently of "a" which is why increment only affected "b"
             Console.WriteLine(String.Format("a: {0}, b: {1}", a, b));
+
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            //array1 and array2 have the same memory address, so any changes to the object on the heap will impact both of them.
+            Console.WriteLine(String.Format("array1[0]: {0}, array2[0]: {0}", array1[0], array2[0]));
         }
     }
 }
